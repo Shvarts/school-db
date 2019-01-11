893f1bf9a70bae556f8865ba145c8cea1152cbbb
Oleg Shvarz <oscwartz@gmail.com>
2019-01-11 02:14:33 +0200

    Updates

---
+++ precache-manifest.a8b85b2eda1f879211dd9c7a59e9d91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8ea3251075b7fb5799a",
+    "url": "/school-db/static/js/runtime~main.e8ea3251.js"
+  },
+  {
+    "revision": "ddf3c249bfdc5f93f351",
+    "url": "/school-db/static/js/main.ddf3c249.chunk.js"
+  },
+  {
+    "revision": "c1c76695518a9a8c455e",
+    "url": "/school-db/static/js/1.c1c76695.chunk.js"
+  },
+  {
+    "revision": "ddf3c249bfdc5f93f351",
+    "url": "/school-db/static/css/main.99c64dda.chunk.css"
+  },
+  {
+    "revision": "c1c76695518a9a8c455e",
+    "url": "/school-db/static/css/1.9c501c80.chunk.css"
+  },
+  {
+    "revision": "d05b99b64f0b42754883447dd64f9f91",
+    "url": "/school-db/index.html"
+  }
+];
